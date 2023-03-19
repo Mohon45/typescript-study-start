f2902ab2be603e310967daee966b971c0de6e2e9
Md: Mohon <mdmohon6145@gmail.com>
2023-03-19 11:30:07 +0600

    typescript start mission

---
+++ intro/index.js
@@ -0,0 +1,9 @@
+var userName = "Md. Mohon";
+// console.log(userName);
+var age = 22;
+// console.log(age);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(5, 10);
+addNumbers(5, 10);
---
+++ intro/index.ts
@@ -0,0 +1,12 @@
+let userName = "Md. Mohon";
+// console.log(userName);
+
+let age = 22;
+// console.log(age);
+
+function addNumbers(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+
+addNumbers(5, 10);
+addNumbers(5, 10);
